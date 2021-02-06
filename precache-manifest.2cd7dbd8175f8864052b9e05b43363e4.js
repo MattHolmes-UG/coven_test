38912eb237abff600ca3f2baab763aebb4c24c6d
MattHolmes <matthewochonogorug@gmail.com>
2021-02-06 16:44:33 +0100

    Updates

---
+++ precache-manifest.2cd7dbd8175f8864052b9e05b43363e4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b473cefe7a19156b12aff1743394d6a8",
+    "url": "/coven_test/index.html"
+  },
+  {
+    "revision": "e2106efca30d9c528b1f",
+    "url": "/coven_test/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "2d9f47a12b607cb0c8dc",
+    "url": "/coven_test/static/css/main.2411edf3.chunk.css"
+  },
+  {
+    "revision": "e2106efca30d9c528b1f",
+    "url": "/coven_test/static/js/2.e593260f.chunk.js"
+  },
+  {
+    "revision": "e9b70ed94c92560626f6d789c6814c32",
+    "url": "/coven_test/static/js/2.e593260f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d9f47a12b607cb0c8dc",
+    "url": "/coven_test/static/js/main.3a41f094.chunk.js"
+  },
+  {
+    "revision": "bd13b6e04570b7f1a649",
+    "url": "/coven_test/static/js/runtime-main.8bd67dcf.js"
+  },
+  {
+    "revision": "48b6c0374ed29a441a870a98374a2433",
+    "url": "/coven_test/static/media/pexels-photo-723240.48b6c037.jpeg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/coven_test/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/coven_test/static/media/sidebar-2.310509c9.jpg"
+  }
+]);
